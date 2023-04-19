f0770676654f2815c067d18681364227236ffa53
Agnes-Kalunda <agypeter97@gmail.com>
2023-04-19 20:24:28 +0300

    task_detail view

--- backend/backendApp/views.py
+++ backend/backendApp/views.py
@@ -53,3 +53,22 @@ def task_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
